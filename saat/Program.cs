1ab52c968c65a2da7093dd5e395c3da4c37088e1
ma.ahmadi <m83115151@gmail.com>
2023-06-26 09:56:10 +0430

    Add project files.

---
+++ saat/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace saatee
+{
+    class zaman
+    {
+        private int hours;
+        private int minutes;
+        public int Hours
+        {
+            get { return hours; }
+            set
+            {
+                if (value > 0)
+                {
+                    hours = value;
+                }
+            }
+        }
+        public int Minuts
+        {
+            get{ return minutes; }
+            set
+            {
+                if (value > 0)
+                {
+                    minutes = value;
+                }
+            }
+        }
+        public zaman() { }
+        public zaman(int Hours,int Minutes ) 
+        {
+            minutes = Minutes;
+            hours = Hours;
+        }
+        public void saat()
+        {
+            if (hours <= 0 || minutes <= 0)
+            {
+                Console.WriteLine("eror");
+            }
+            else if (hours > 12 && hours < 24)
+            {
+                Console.WriteLine("pm");
+            }
+            else
+            {
+                Console.WriteLine("am");
+            }
+           
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hours");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("minutes");
+            int minutes = Convert.ToInt32(Console.ReadLine());
+            zaman ts = new zaman(hours,minutes);
+            ts.saat();
+        }
+    }
+}
